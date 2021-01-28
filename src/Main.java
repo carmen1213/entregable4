59fbfcdf73601b5a7d9dc239fcb6f4c9364fbe4b
Carmen <cmarti@campuscamarafp.com>
2021-01-28 13:06:31 +0100

    ventanas

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String exePath = "C:\\Users\\Carmen\\IntelliJProjects\\Entregable4\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", exePath);
         ChromeOptions options = new ChromeOptions();
@@ -12,15 +18,28 @@ public class Main {
         ChromeDriver driver = new ChromeDriver(options);
         driver.get("http://www.fnac.es");
         // driver.quit();}
-        WebElement ventanaCookies =
-                driver.findElement(By.xpath("/html/body/header/div/button"));
-        if (ventanaCookies !=null){System.out.println("Detectado caja decookies");
-        ventanaCookies.click();}
+        //WebElement ventanaCookies =
+                //driver.findElement(By.xpath("/html/body/header/div/button"));
+       // if (ventanaCookies !=null){System.out.println("Detectado caja decookies");
+            //ventanaCookies.click();}
+        System.out.println("Que producto quieres buscar: ");
+        String producto= sc.nextLine();
+
         WebElement cajaBusqueda =  driver.findElement(By.id("Fnac_Search"));
-        cajaBusqueda.sendKeys("Moviles");
+        cajaBusqueda.sendKeys(producto);
         cajaBusqueda.submit();
 
-        System.out.println("Que producto quieres buscar: ");
+
+        WebElement elementoActual, navegacion;
+        int j=1;
+        for (int i=1; i<4;i++){
+            String modelo = driver.findElement(By.xpath("/html/body/div[3]/div/div[7]/div/div["+ i +"]/article/div[2]/div/p[1]/a")).getText();
+            String precio =driver.findElement(By.xpath("/html/body/div[3]/div/div[7]/div/div["+ i +"]/article/div[3]/div/div/div/div/div[1]/a/strong")).getText();
+            System.out.println(modelo);
+            System.out.println(precio);
+        }
+
+
 
 
     }
