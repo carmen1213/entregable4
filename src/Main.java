a6e89a943b002d6c2418d0bc6dd2b366bd590a7c
Carmen <cmarti@campuscamarafp.com>
2021-01-29 13:16:41 +0100

    ventanas

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.Scanner;
@@ -10,37 +12,88 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String exePath = "C:\\Users\\Carmen\\IntelliJProjects\\Entregable4\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver", exePath);
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.get("http://www.fnac.es");
-        // driver.quit();}
-        //WebElement ventanaCookies =
-                //driver.findElement(By.xpath("/html/body/header/div/button"));
-       // if (ventanaCookies !=null){System.out.println("Detectado caja decookies");
-            //ventanaCookies.click();}
-        System.out.println("Que producto quieres buscar: ");
-        String producto= sc.nextLine();
-
-        WebElement cajaBusqueda =  driver.findElement(By.id("Fnac_Search"));
-        cajaBusqueda.sendKeys(producto);
-        cajaBusqueda.submit();
+        System.out.println("dime el telefono que quieras");
+        String telefono=sc.nextLine();
+       Phonehouses(telefono);
+        System.out.println("Presiona enter para comparar precios en las dos paginas");
+        sc.nextLine();
+        PcComponentes(telefono);
 
 
-        WebElement elementoActual, navegacion;
-        int j=1;
-        for (int i=1; i<4;i++){
-            String modelo = driver.findElement(By.xpath("/html/body/div[3]/div/div[7]/div/div["+ i +"]/article/div[2]/div/p[1]/a")).getText();
-            String precio =driver.findElement(By.xpath("/html/body/div[3]/div/div[7]/div/div["+ i +"]/article/div[3]/div/div/div/div/div[1]/a/strong")).getText();
-            System.out.println(modelo);
-            System.out.println(precio);
-        }
 
 
 
 
     }
+    public static void Phonehouses(String telefono){
+        Scanner sc = new Scanner(System.in);
+        String exePath = "C:\\Users\\Carmen\\IntelliJProjects\\Entregable4\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", exePath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://www.phonehouse.es/moviles-y-telefonia/moviles/todos-los-smartphones.html");
+
+
+
+
+        WebElement cajaBusqueda =  driver.findElement(By.id("searcher"));
+        cajaBusqueda.sendKeys(telefono);
+        cajaBusqueda.submit();
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Estas en la pagina de PhoneHouse");
+        int j=1;
+        for (int i=1; i<4;i++){
+            String modelo=driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div/section[2]/div[2]/ul/div/div/li["+i+"]/div/div[1]/div[2]/div[1]/a/h3")).getText();
+            String precio =driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div/section[2]/div[2]/ul/div/div/li["+ i +"]/div/div[2]/div[1]/div/div[2]/span")).getText();
+            System.out.println(modelo);
+            System.out.println(precio);
+            System.out.println("************************************************************************************");
+        }
+
+
+    }
+    public static void PcComponentes(String telefono){
+        Scanner sc = new Scanner(System.in);
+        String exePath = "C:\\Users\\Carmen\\IntelliJProjects\\Entregable4\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", exePath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://www.pccomponentes.com/smartphones-gps");
+
+        System.out.println("Que producto quieres buscar: ");
+        String producto= sc.nextLine();
+
+
+        WebElement cajaBusqueda =  driver.findElement(By.id("searcher"));
+        cajaBusqueda.sendKeys(telefono);
+        cajaBusqueda.submit();
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Estas en la pagina de PhoneHouse");
+        int j=1;
+        for (int i=1; i<4;i++){
+            String modelo=driver.findElement(By.xpath("")).getText();
+            String precio =driver.findElement(By.xpath("")).getText();
+            System.out.println(modelo);
+            System.out.println(precio);
+            System.out.println("************************************************************************************");
+        }
+
+
+    }
+
+
 }
